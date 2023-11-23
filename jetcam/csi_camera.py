f3b92339e3dea9ed4b99e7ad40f4b1094f986c04
StemPlusLeslie <127295527+StemPlusLeslie@users.noreply.github.com>
2023-11-23 23:02:07 +0800

    untest csi camera mux

--- jetcam/csi_camera.py
+++ jetcam/csi_camera.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import threading
 import traitlets
+import os
+import socket
 
 
 class CSICamera(Camera):
@@ -15,16 +17,24 @@ class CSICamera(Camera):
     
     def __init__(self, *args, **kwargs):
         super(CSICamera, self).__init__(*args, **kwargs)
+        self.sock_addr = '/tmp/csi_socket'
         try:
-            self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
-
-            re, image = self.cap.read()
-
-            if not re:
-                raise RuntimeError('Could not read image from camera.')
+            self._init_csi()
         except:
-            raise RuntimeError(
-                'Could not initialize camera.  Please see error trace.')
+            self._remote_close()
+            try:
+                self._init_csi()
+            except:
+                raise RuntimeError('Could not initialize camera.  Please see error trace.')
+        try:
+            os.remove(self.sock_addr)
+        except OSError:
+            pass
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.bind(self.sock_addr)
+        self.sock.listen(1)
+        self.sock_recv_thread = threading.Thread(target=self._sock_recv)
+        self.sock_recv_thread.start()
 
         atexit.register(self.cap.release)
                 
@@ -38,3 +48,33 @@ class CSICamera(Camera):
             return image
         else:
             raise RuntimeError('Could not read image from camera')
+            
+    def _init_csi(self):
+        self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
+        re, image = self.cap.read()
+        if not re:
+            raise RuntimeError('Could not read image from camera.')
+            
+    def _sock_recv(self):
+        conn, addr = self.sock.accept()
+        while True:
+            recv_data = conn.recv(1024).decode()
+            if recv_data == "kill":
+                self.cap.release()
+                self.sock.sendall("is all yours".encode())
+                conn.close()
+                self.sock.close()
+                break
+            
+    def _remote_close(self):
+        # try to connect other
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            self.sock.connect(self.sock_addr)
+        except:
+            raise RuntimeError('Could not connect to other python kernel that using the CSI camera')
+        self.sock.sendall("kill".encode())
+        recv_data = self.sock.recv(1024).decode()
+        if recv_data != "is all yours":
+            raise RuntimeError("Failed to close other, message: " + recv_data)
+        self.sock.close()
